2519953111749388271be28ed7ded0737467382b
miles <813601376@qq.com>
2021-05-27 14:11:18 +0800

    update

---
+++ dist/LoadExcelMac.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# 使用脚本可以将json和ts,cs文件复制到指定目录
+
+jsonPath=../assets/resources/data/
+entityPath=../assets/script/game/
+
+./LoadExcel ts[cs]
+
+files=$(ls ./out)
+for filename in $files; do
+    ext=${filename#*.}
+    if [ $ext == "json" ]; then
+        cp -R ./out/$filename $jsonPath
+    fi
+    if [ $ext == "cs" ] || [ $ext == "ts" ]; then
+        cp -R ./out/$filename $entityPath
+    fi
+done
