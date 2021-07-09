cd6563ce93061cc24c34a0a0f55f0bbd1d142b9b
miles <813601376@qq.com>
2021-07-09 15:12:01 +0800

    update

--- README.md
+++ README.md
@@ -4,11 +4,16 @@
 一个导表工具，可以将Excel表导出为json数据，并同时导出数据表interface文件方便TypeScript和C#环境使用。 
 
 #### 环境
-nodejs + typescript + exceljs + pkg(导出为各系统执行文件)  
+nodejs + typescript + exceljs + pkg(导出为各系统执行文件)
+
+#### 运行
+直接运行: npm start cs|ts  
+打包为执行文件: npm run  pkg
 
 #### 说明
 1、工具会检测当前目录及子目录所有Excel文件，Excel名字中包含～的文件会被忽略  
 2、每个Excel表所有sheet数据会导出在同一个Json文件，文件名使用Excel文件名  
 3、在LoadExcel中配置 Type:字段类型所在行 Key:字段key所在行 Commit:字段批注所在行 DataStart:数据开始的行 (行数从1开始)  
-4、TypeScript数据类型支持：number，string，boolean以及它们的一维二维数组(一维[],二维[][])；boolean用1、0表示true、false；一维数组使用(;)分隔,二维数组使用(,)分隔 ；字段类型不填表示此列数据将不会被导出。  
-5、C#数据类型支持：int，float，string，bool以及它们的一维二维数组(一维[],二维[][])；bool用1、0表示true、false；一维数组使用(;)分隔,二维数组使用(,)分隔 ；字段类型不填表示此列数据将不会被导出。  
+4、TypeScript数据类型支持：number，string，boolean以及它们的一维二维数组  
+5、C#数据类型支持：int，float，string，bool以及它们的一维二维数组  
+6、一维数组字段类型后面加[],二维数组字段类型后面加[][]；boolean用1、0表示true、false；一维数组使用(;)分隔,二维数组使用(,)分隔；字段类型不填表示此列数据将不会被导出。    
--- src/LoadToCS.ts
+++ src/LoadToCS.ts
@@ -1,5 +1,110 @@
-export class LoadToCS {
-    static load(xlsxs: { [name: string]: string }) {
+import excel from "exceljs";
+import { LoadExcel } from "./LoadExcel";
 
+export class LoadToCS {
+    static async load(xlsxs: { [name: string]: string }) {
+        let entityHeader = "using System.Collections;\nusing System.Collections.Generic;\n\n";
+        let entityResult = "";
+        for (const name in xlsxs) {
+            let dataResult: { [name: string]: object } = {}
+            entityHeader += "public class " + name + "   \n{\n"
+            let wbResult = await this.readExcel(xlsxs[name])
+            entityResult += wbResult["entityStr"]
+            for (const key in wbResult["wbDict"]) {
+                dataResult[key] = wbResult["wbDict"][key]
+                entityHeader += ("    public Dictionary<string," + key + "> " + key + ";\n")
+            }
+            entityHeader += "}\n\n"
+            LoadExcel.saveData(JSON.stringify(dataResult), name + ".json")
+        }
+        LoadExcel.saveData(entityHeader + entityResult, "DataEntity.cs")
+    }
+
+    static async readExcel(filePath: string): Promise<{ entityStr: string, wbDict: { [name: string]: { [key: string]: any } } }> {
+        let workbook = new excel.Workbook();
+        await workbook.xlsx.readFile(filePath)
+        let sheets = workbook.worksheets;
+        let wbDict: { [name: string]: { [key: string]: any } } = {}
+        let entityStr: string = ""
+        for (const sheet of sheets) {
+            console.log("load sheet " + sheet.name + " start")
+            let sName = LoadExcel.upperFirst(sheet.name)
+            wbDict[sName] = {}
+            let sheetDict = wbDict[sName]
+            let typeList: string[] = []
+            let keyList: string[] = []
+            let commitList: string[] = []
+            for (let row = 1; row < sheet.rowCount; row++) {
+                let id = ""
+                for (let col = 1; col < sheet.columnCount; col++) {
+                    let value = sheet.getCell(row, col).value;
+                    let cellV = value ? value.toString() : "";
+                    if (row == LoadExcel.rowNum.Type) {
+                        typeList[col] = cellV;
+                    }
+                    if (row == LoadExcel.rowNum.Key) {
+                        keyList[col] = cellV;
+                    }
+                    if (row == LoadExcel.rowNum.Commit) {
+                        commitList[col] = cellV;
+                    }
+                    if (row >= LoadExcel.rowNum.DataStart) {
+                        if (col == 1 && cellV) {
+                            sheetDict[cellV] = {}
+                            id = cellV;
+                        }
+                        if (!id || !typeList[col] || typeList[col] == "none" || !keyList[col]) continue;
+                        sheetDict[id][keyList[col]] = this.getValueByType(cellV, typeList[col])
+                    }
+                }
+            }
+            entityStr += "public class " + sName + "  \n{\n"
+            if (keyList.length == typeList.length) {
+                for (let i = 0; i < typeList.length; i++) {
+                    let type = typeList[i]
+                    if (!type || type == "none") continue;
+                    entityStr += "    /// <summary>\n"
+                    entityStr += "    /// " + commitList[i] + "\n"
+                    entityStr += "    /// <summary>\n"
+                    entityStr += "    public " + typeList[i] + " " + keyList[i] + ";\n"
+                }
+            }
+            entityStr += "}\n\n"
+            console.log("load sheet " + sheet.name + " end")
+        }
+        return { "entityStr": entityStr, "wbDict": wbDict };
+    }
+
+    static getValueByType(cellV: string, type: string): any {
+        cellV = cellV.trim()
+        let result: any;
+        if (type.includes("bool")) {
+            if (type == "bool[][]") {
+                result = cellV.split(";").filter(v => v).map(v => v.split(",").filter(v => v).map(v => v == "1" ? true : false));
+            } else if (type == "bool[]") {
+                result = cellV.split(";").filter(v => v).map(v => v == "1" ? true : false);
+            } else {
+                result = cellV == "1" ? true : false;
+            }
+        } else if (type.includes("int") || type.includes("float")) {
+            if (type == "int[][]" || type == "float[][]") {
+                result = cellV.split(";").filter(v => v).map(v => v.split(",").filter(v => v).map(v => LoadExcel.parseNum(v)));
+            } else if (type == "int[]" || type == "float[]") {
+                result = cellV.split(";").filter(v => v).map(v => LoadExcel.parseNum(v));
+            } else {
+                result = LoadExcel.parseNum(cellV)
+            }
+        } else if (type.includes("string")) {
+            if (type == "string[][]") {
+                result = cellV.split(";").filter(v => v).map(v => v.split(",").filter(v => v));
+            } else if (type == "string[]") {
+                result = cellV.split(";").filter(v => v);
+            } else {
+                result = cellV
+            }
+        } else {
+            console.log("不支持的数据类型", type)
+        }
+        return result;
     }
 }
